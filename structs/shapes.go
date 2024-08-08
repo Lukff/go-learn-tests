086f95839714b03ca98119fec63f8e6866447545
Lucas Fernandes <dlukff@gmail.com>
2024-08-08 11:20:24 -0300

    feat: implement perimeter for shapes

--- structs/shapes.go
+++ structs/shapes.go
@@ -4,6 +4,7 @@ import "math"
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -15,6 +16,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Heigth
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Heigth)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,15 +28,21 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Triangle struct {
-	Base   float64
-	Heigth float64
+	A float64
+	B float64
+	C float64
 }
 
 func (t Triangle) Area() float64 {
-	return (t.Base * t.Heigth) * 0.5
+	s := (t.A + t.B + t.C) * 0.5
+	return math.Sqrt(s * (s - t.A) * (s - t.B) * (s - t.C))
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Heigth)
+func (t Triangle) Perimeter() float64 {
+	return t.A + t.B + t.C
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,12 +5,24 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	perimeterTests := []struct {
+		name         string
+		shape        Shape
+		hasPerimeter float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10, 10}, hasPerimeter: 40},
+		{name: "Circle", shape: Circle{5}, hasPerimeter: 31.41592653589793},
+		{name: "Triangle", shape: Triangle{A: 3, B: 4, C: 5}, hasPerimeter: 12.0},
+	}
+
+	for _, tt := range perimeterTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.hasPerimeter {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
@@ -23,7 +35,7 @@ func TestArea(t *testing.T) {
 	}{
 		{name: "Rectangle", shape: Rectangle{Width: 12, Heigth: 6}, hasArea: 72.0},
 		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{Base: 12, Heigth: 6}, hasArea: 36.0},
+		{name: "Triangle", shape: Triangle{A: 3, B: 4, C: 5}, hasArea: 6.0},
 	}
 
 	for _, tt := range areaTests {
