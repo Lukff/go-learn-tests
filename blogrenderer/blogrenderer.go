893c2733305d7017b6be0688ea7d5fb00cbb3a76
Lucas Fernandes <dlukff@gmail.com>
2024-09-27 11:44:26 -0300

    feat: add blog post renderer

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,34 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	blogposts "learngowithtests/blogposts"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []blogposts.Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
